b329ccaa176f6b3f0f8323cd6bf90c2eb2271408
Killa J <joshuaalexandercross@gmail.com>
2021-05-02 23:40:06 -0500

    started initializing databases with schema and seeds

---
+++ db/schema.sql
@@ -0,0 +1,42 @@
+DROP TABLE IF EXISTS candidates;
+DROP TABLE IF EXISTS parties;
+DROP TABLE IF EXISTS voters;
+DROP TABLE IF EXISTS votes;
+
+CREATE TABLE departments (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  name VARCHAR(50) NOT NULL,
+);
+
+CREATE TABLE employees (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INTEGER(10) NOT NULL,
+  manager_id INTEGER(10),
+  CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
+  CONSTRAINT fk_manager FOREIGN KEY (manager_id) REFERENCES managers(id) ON DELETE SET NULL
+);
+
+CREATE TABLE roles (
+  id INTEGER AUTO_INCREMENT PRIMARY KEY,
+  title VARCHAR(30) NOT NULL,
+  salary DECIMAL(10,2) NOT NULL,
+  department_id INTEGER,
+  CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE SET NULL
+);
+
+CREATE TABLE managers (
+    id INTEGER AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR (30) NOT NULL
+)
+
+-- CREATE TABLE votes (
+--   id INTEGER AUTO_INCREMENT PRIMARY KEY,
+--   voter_id INTEGER NOT NULL,
+--   candidate_id INTEGER NOT NULL,
+--   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+--   CONSTRAINT uc_voter UNIQUE (voter_id),
+--   CONSTRAINT fk_voter FOREIGN KEY (voter_id) REFERENCES voters(id) ON DELETE CASCADE,
+--   CONSTRAINT fk_candidate FOREIGN KEY (candidate_id) REFERENCES candidates(id) ON DELETE CASCADE
+-- );
---
+++ db/seeds.sql
@@ -0,0 +1,22 @@
+INSERT INTO departments (name)
+VALUES
+    ('Marketing'),
+    ('Sales'),
+    ('Administration'),
+    ('Human Resources'),
+    ('Customer Service'),
+    ('Operations'),
+    ('IT');
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES
+    ('Ronald', 'Firbank', 0, 1),
+    ('Virginia', 'Woolf', 0, 1),
+    ('Piers', 'Gaveston', 1, 0),
+    ('Charles', 'LeRoi', 1, 1),
+    ('Katherine', 'Mansfield', 2, 1),
+    ('Dora', 'Carrington', 2, 0),
+    ('Edward', 'Bellamy', 3, 0),
+    ('Montague', 'Summers', 3, 1),
+    ('Octavia', 'Butler', 3, 1),
+    ('Joshua', 'Cross', 0, )
