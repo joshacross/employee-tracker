438a1c40df281be865dcebe37bf2b0e4e0bb8ca3
Killa J <joshuaalexandercross@gmail.com>
2021-05-03 23:58:26 -0500

    started app.js file and wrote preliminary inquirer prompts

--- db/schema.sql
+++ db/schema.sql
@@ -1,7 +1,6 @@
-DROP TABLE IF EXISTS departments;
-DROP TABLE IF EXISTS roles;
-DROP TABLE IF EXISTS employees;
-DROP TABLE IF EXISTS managers;
+DROP DATABASE IF EXISTS employee;
+CREATE DATABASE employee;
+USE employee;
 
 -- Departments Table
 CREATE TABLE departments (
@@ -18,12 +17,6 @@ CREATE TABLE roles (
   CONSTRAINT fk_department FOREIGN KEY (department_id) REFERENCES departments(id) ON DELETE SET NULL
 );
 
--- Dependent table for managers
-CREATE TABLE managers (
-    id INTEGER AUTO_INCREMENT PRIMARY KEY,
-    last_name VARCHAR (30) NOT NULL,
-);
-
 -- Employee Table
 CREATE TABLE employees (
   id INTEGER AUTO_INCREMENT PRIMARY KEY,
@@ -34,8 +27,8 @@ CREATE TABLE employees (
   managers_id INTEGER(10),
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES roles(id) ON DELETE SET NULL,
-  CONSTRAINT fk_managers FOREIGN KEY (managers_id) REFERENCES managers(id) ON DELETE CASCADE,
-  CONSTRAINT fk_managers_name FOREIGN KEY (manager) REFERENCES managers(last_name) ON DELETE SET NULL
+  CONSTRAINT fk_managers FOREIGN KEY (managers_id) REFERENCES employees(id) ON DELETE CASCADE,
+  CONSTRAINT fk_managers_name FOREIGN KEY (manager) REFERENCES employees(last_name) ON DELETE SET NULL
 );
 
 
