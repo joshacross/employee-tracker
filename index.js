9e2d3b641d0b2611338e4db29fb81ec961a5dc5d
Killa J <joshuaalexandercross@gmail.com>
2021-07-19 19:13:07 -0500

    added addEmployee, updateEmployee, and updateRoles functions and tested application

--- index.js
+++ index.js
@@ -1,15 +1,26 @@
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
 const cTable = require('console.table');
-const express = require('express');
-const db = require('./db/connection');
+require('dotenv').config();
+
+// Connect to Database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // Your mySQL username,
+        user: 'root',
+        // Your mySQL pwd
+        password: process.env.DB_PW,
+        database: 'employee'
+    },
+    console.log('Connected to the employee database.')
+);
 
 const employeeTracker = () => {
     inquirer
-        .prompt([
-            {
+        .prompt({
                 name: 'action',
-                type: 'rawlist',
+                type: 'list',
                 message: "What would you like to do?",
                 choices: [
                     'View All Departments', 
@@ -20,8 +31,7 @@ const employeeTracker = () => {
                     'Add an Employee', 
                     'Update an Employee Role'
                 ],
-            }
-        ])
+        })
         .then((answer) => {
             switch (answer.action) {
                 case 'View All Departments':
@@ -44,8 +54,12 @@ const employeeTracker = () => {
                     addEmployee();
                     break;
                 
-                case 'Update an Employee Role':
+                case 'Update an Employee Record':
                     updateEmployee();
+                    break;
+                
+                case 'Add a Role':
+                    updateRoles();
                     break;
             }
         });
@@ -96,24 +110,17 @@ const viewEmployees = () => {
 
 
 const addDepartment = () => {
+
     inquirer
         .prompt([
             {
                 name: 'addDepartment',
                 type: 'input',
                 message: 'What department would you like to add?',
-                validate: nameInput => {
-                    if (nameInput) {
-                        return true;
-                    } else {
-                        console.log('Please enter a valid department name');
-                        return;
-                    }
-                }
             }
         ])
-        .then(body => {
-            const sql = `INSERT INTO departments (department_name) VALUES ?`;
+        .then(body = (response) => {
+            const sql = `INSERT INTO departments (department_name) VALUES ('${response.addDepartment}');`;
             const param = [body.addDepartment];
             db.query(sql, param, (err, res) => {
                 if (err) {
@@ -125,66 +132,149 @@ const addDepartment = () => {
         });
 };
 
+// Function to add employment
 const addEmployee = () => {
+    console.log("Add an employee");
+    // Inquirer prompt to ask for title, salary and department id of new role,
     inquirer
-        .prompt([
-            {
-                name: 'employeeName',
-                type: 'input',
-                message: 'What is the employees first name?'
-                // validate: nameInput =>
-                // validate: nameInput => {
-                //     if (nameInput) {
-                //         return true;
-                //     } else {
-                //         console.log('Please enter a valid department name');
-                //         return false;
-                //     }
-                // }
-            }
-        ])
-        .then()
-    console.log('Add Employee Script');
-    employeeTracker();
-    return;
-};
+      .prompt([
+        {
+          name: "newEmpFirstName",
+          type: "input",
+          message: "What is the new employee's first name?",
+        },
+        {
+          name: "newEmpLastName",
+          type: "input",
+          message: "What is the new employee's last name?",
+        },
+        {
+          name: "newEmpRoleID",
+          type: "number",
+          message: "What is the role ID for your new employee?",
+        },
+        {
+          name: "newEmpManagerID",
+          type: "number",
+          message: "What is the manager ID for your new employee?",
+        },
+      ])
+      .then(function (answer) {
+        db.query(
+          `INSERT INTO employee (first_name, last_name, role_id, manager_id) VALUES ("${answer.newEmpFirstName}", "${answer.newEmpLastName}", "${answer.newEmpSalary}", "${answer.newEmpRoleID}", "${answer.newEmpManagerID}");`,
+          function (err, data) {
+            // Then connection.query to INSERT INTO department (name) VALUE ("userInput")
+            // Console.table new department table
+            console.log("Employee has been added.");
+          }
+        );
+        viewEmployees();
+      });
+  }
+    
+
 
 const updateEmployee = () => {
-    console.log('Update Employee Script');
-    employeeTracker();
-    return;
-};
+// Function to update employee record
+    console.log("Update an employee record");
+  
+    // Get a list of employees
+    db.query("SELECT * FROM employees;", function (err, data) {
+        console.table(data);
+        // display list to user
+          inquirer
+          .prompt([
+            {
+            name: "updateEmployee",
+            type: "input",
+            message: "Enter the id of the employee would you like to update.",
+          },
+          {
+            name: "updateInfo",
+            type: "list",
+            message: "What information would you like to update?",
+            choices: [
+                "first_name",
+                "last_name",
+                "role_id",
+                "manager_id",
+              ],
+          },
+          {
+              name: "updateInput",
+              type: "input",
+              message: "What is the new value?",
+            }
+        ])
+          .then(function (answer) {
+              // console.log('!!!!!!!!!!!!!!!!!!!!!!', answer);
+              let query = `
+                UPDATE employees 
+                SET ${answer.updateInfo} = '${answer.updateInput}'
+                WHERE id = ${answer.updateEmployee};`
+              console.log(query)
+            db.query(
+              query,
+              function (err, data) {
+                // Then connection.query to INSERT INTO department (name) VALUE ("userInput")
+                // Console.table new department table
+                if (err) {
+                    console.log(err)
+                    return;
+                } else {
+                    console.log("Employee has been updated.");
+                    viewEmployees();
+                    return;
+                }
+              }
+            )}
+        )
+    });
+  }
+
+// Function to add roles
+const updateRoles = () => {
+    console.log("Update Roles");
+
+    db.query("SELECT * FROM roles;", function (err, data) {
+        console.table(data);
+        // display list to user
+          inquirer
+          .prompt([
+    // Inquirer prompt to ask for title, salary and department id of new role
+        {
+          name: "newRoleTitle",
+          type: "input",
+          message: "What is the job title for your new role?",
+        },
+        {
+          name: "newRoleSalary",
+          type: "number",
+          message: "What is the salary for your new role?",
+        },
+        {
+            // NEEDS TO BE UPDATED TO INCLUDE LIST OF DEPARTMENTS
+          name: "newRoleDepartmentID",
+          type: "number",
+          message: "What is the department ID for your new role?",
+        },
+      ])
+        .then(function (answer) {
+            db.query(
+          `INSERT INTO roles (title, salary, department_id) VALUES ("${answer.newRoleTitle}", "${answer.newRoleSalary}", "${answer.newRoleDepartmentID}");`,
+          function (err, data) {
+            if (err) {
+                console.log(err);
+                return;
+            } else {
+                console.log("A New Role has been added.");
+                viewRoles();
+                return;
+            }
+          }
+        );
+      });
+    }
+)}
 
 employeeTracker();
-module.exports = employeeTracker;
-
-
-
-// Inquirer - selected >>
-
-// Run SQL Script to show table information based upon request
-
-// 
-
-// USER STORY
-// AS A business owner
-// I WANT to be able to view and manage the departments, roles, and employees in my company
-// SO THAT I can organize and plan my business
-
-// GIVEN a command-line application that accepts user input
-// WHEN I start the application
-// THEN I am presented with the following options: view all departments, view all roles, view all employees, add a department, add a role, add an employee, and update an employee role
-// WHEN I choose to view all departments
-// THEN I am presented with a formatted table showing department names and department ids
-// WHEN I choose to view all roles
-// THEN I am presented with the job title, role id, the department that role belongs to, and the salary for that role
-// WHEN I choose to view all employees
-// THEN I am presented with a formatted table showing employee data, including employee ids, first names, last names, job titles, departments, salaries, and managers that the employees report to
-// WHEN I choose to add a department
-// THEN I am prompted to enter the name of the department and that department is added to the database
-// WHEN I choose to add a role
-// THEN I am prompted to enter the name, salary, and department for the role and that role is added to the database
-// WHEN I choose to add an employee
-// THEN I am prompted to enter the employee’s first name, last name, role, and manager and that employee is added to the database
-// WHEN I choose to update an employee role
-// THEN I am prompted to select an employee to update and their new role and this information is updated in the database 
